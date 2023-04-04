9070f8b64b8783e0692a0528ffe5e090b05f53c1
alibw <69758323+alibw@users.noreply.github.com>
2023-04-04 13:36:26 +0330

    Init

---
+++ DDLDumper/Dumper.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace DDLDumper;
+
+public static class Dumper
+{
+    public static string ToSql(this Table table, bool indentation = true)
+    {
+        return Dump(table,GetColumns,GetPrimaryKey,GetForeignKeys,indentation);
+    }
+
+    public static string Dump(Table table,Func<List<Column>,string> getColumns,Func<Table,string>getPrimaryKey,Func<Table,string>getForeignKeys, bool indentation)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append
+        (
+$@"USE [{table.DbName}]
+CREATE TABLE {table.TableName}
+(
+{(indentation ? getColumns(table.Properties).WithIndent() : GetColumns(table.Properties))}
+)
+{getPrimaryKey(table)}
+{getForeignKeys(table)}");
+        return sb.ToString();
+    }
+
+    public static Func<Table, string> GetPrimaryKey = table =>
+    {
+        var primaryKey = table.Properties.FirstOrDefault(x => x.IsPrimaryKey);
+        var primaryKeyLine = $@"ALTER TABLE [dbo].[{table.TableName}] ADD CONSTRAINT [PK_{table.TableName}] PRIMARY KEY CLUSTERED ([{primaryKey.Name}])";
+        return primaryKeyLine;
+    };
+
+    public static Func<List<Column>, string> GetColumns = columns =>
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (var column in columns)
+        {
+            sb.Append($@"[{column.Name}] [{column.Type}] {(column.Nullable ? "NULL" : "NOT NULL")}{(column == columns.Last() ? "" : ",\r\n")}");
+        }
+
+        return sb.ToString();
+    };
+
+    public static Func<Table, string> GetForeignKeys = table =>
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (var column in table.Properties.Where(x => x.IsForeignKey))
+        {
+            sb.Append($@"ALTER TABLE [dbo].[{table.TableName}] ADD CONSTRAINT [FK_{table.TableName}_{column.ForeignKeyTable.TableName}] FOREIGN KEY ([{column.Name}]) REFERENCES [dbo].[{column.ForeignKeyTable.TableName}] ([{column.ForeignKeyTable.Properties.FirstOrDefault(x => x.IsPrimaryKey).Name}])");
+        }
+
+        return sb.ToString();
+    };
+}
---
+++ DDLDumper/Test.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+
+namespace DDLDumper;
+
+[TestFixture]
+public class Test
+{
+        Table table = new Table()
+            {
+                DbName = "College",
+                TableName = "Lesson",
+                Properties = new List<Column>()
+                {
+                    new Column
+                    {
+                        Name = "Id",
+                        Type = "bigint",
+                        IsIdentity = true,
+                        IsPrimaryKey = true
+                    },
+                    new Column
+                    {
+                        Name = "Title",
+                        Type = "nvarchar"
+                    },
+                    new Column
+                    {
+                        Name = "UnitNum",
+                        Type = "bigint"
+                    },
+                    new Column
+                    {
+                        Name = "MasterId",
+                        Type = "bigint",
+                        IsForeignKey = true,
+                        ForeignKeyTable = new Table()
+                        {
+                            DbName = "College",
+                            TableName = "Master",
+                            Properties = new List<Column>()
+                            {
+                                new Column
+                                {
+                                    Name = "Id",
+                                    Type = "bigint",
+                                    IsIdentity = true,
+                                    IsPrimaryKey = true
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+    
+    [Test]
+    public void ObjectIntoSql()
+    {
+        var expected = @"USE [College]
+CREATE TABLE Lesson
+(
+[Id] [bigint] NOT NULL,
+[Title] [nvarchar] NOT NULL,
+[UnitNum] [bigint] NOT NULL,
+[MasterId] [bigint] NOT NULL
+)
+ALTER TABLE [dbo].[Lesson] ADD CONSTRAINT [PK_Lesson] PRIMARY KEY CLUSTERED ([Id])
+ALTER TABLE [dbo].[Lesson] ADD CONSTRAINT [FK_Lesson_Master] FOREIGN KEY ([MasterId]) REFERENCES [dbo].[Master] ([Id])";
+        
+    
+        Assert.AreEqual(expected,table.ToSql(false));
+    }
+
+    [Test]
+    public void IndentTest()
+    {
+        var lines =
+@"Line 1
+Line2
+Line3";
+
+        var expected =
+@"  Line 1
+  Line2
+  Line3";
+        
+        Assert.AreEqual(expected, lines.WithIndent());
+    }
+
+    [Test]
+    public void MyFuncTest()
+    {
+        var columns = new List<Column>()
+        {
+            new Column
+            {
+                Name = "Title",
+                Type = "nvarchar"
+            },
+            new Column
+            {
+                Name = "UnitNum",
+                Type = "bigint"
+            }
+        };
+        var result = CustomFunc(Dumper.GetColumns,columns);
+        var expected = @"-- my columns [Title] [nvarchar] NOT NULL,
+[UnitNum] [bigint] NOT NULL"; 
+                       
+        Assert.AreEqual(expected,result);
+    }
+
+    public Func<Func<List<Column>, string>,List<Column>, string> CustomFunc = (getColumns,columns) =>
+    {
+        var result = @$"-- my columns {getColumns(columns)}";
+        return result;
+    };
+}
